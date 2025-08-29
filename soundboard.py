55a326438e03f6cbce651f05f72935ea4ed57555
MokenWoken <davidkingdom225@gmail.com>
2025-08-29 18:02:24 +0200

    Create soundboard.py

---
+++ soundboard.py
@@ -0,0 +1,19 @@
+import pygame
+import keyboard  # requires: pip install keyboard
+
+# init audio
+pygame.mixer.init()
+
+# load sounds
+sound_a = pygame.mixer.Sound("sound_a.wav")
+sound_b = pygame.mixer.Sound("sound_b.wav")
+
+print("Press A or B to play sounds. Press ESC to quit.")
+
+while True:
+    if keyboard.is_pressed("a"):
+        sound_a.play()
+    elif keyboard.is_pressed("b"):
+        sound_b.play()
+    elif keyboard.is_pressed("esc"):
+        break
